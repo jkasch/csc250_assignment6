72d41d966d4cb7d594fa88a855732dc2fc05afa4
jkasch <116533132+jkasch@users.noreply.github.com>
2022-11-15 11:01:45 -0600

    chapter sort

---
+++ assignment6/src/assignment6/BibleBook.java
@@ -0,0 +1,75 @@
+package assignment6;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+	public class BibleBook 
+	{
+        private String book;
+        private int chapters;
+        private String summary;
+
+        public BibleBook (String book, int chapters, String summary)
+        {
+            this.book = book;
+            this.chapters = chapters;
+            this.summary = summary;
+        }
+
+        public int getChapters(){
+            return chapters;
+        }
+
+        public String toString(){
+            return "Book name: "+this.book+"\nChapters: "+this.chapters+"\nSummary: "+this.summary;
+        }
+
+			public static void main(String[] args)
+			{
+                BibleBook[] bookArr = new BibleBook[100];
+                int count=0;
+				try
+				{
+					Scanner fileIn = new Scanner(new File(System.getProperty("user.dir") + "/src/assignment6/input.dat"));
+					while(fileIn.hasNext())
+					{
+						String line = fileIn.nextLine();
+						String array[] = line.split(":");
+						
+							String book = array[0];
+							int chapters = Integer.parseInt(array[1]);
+							String summary = array[2];
+							BibleBook b=new BibleBook(book,chapters,summary);
+                            bookArr[count++] = b;
+							//BibleBook.bubbleSort(BibleBook[] bookArr);
+						//System.out.println("Book name: " + book + "(" + chapters + " chapters) - " + summary);
+					}
+					fileIn.close();
+                    bookArr = bubbleSort(bookArr,count);
+                    for(int i=0;i<count;i++)
+                        System.out.println(bookArr[i]);
+				}
+				catch(Exception e)
+				{
+				System.err.println(e.toString());
+				}
+			}
+			static BibleBook[] bubbleSort(BibleBook[] bookArr,int count)
+			{
+			BibleBook temp;
+				
+				for(int i=0; i < count-1; i++){  
+                 for(int j=0; j < (count-i)-1; j++){  
+					if(bookArr[j].getChapters() > bookArr[j+1].getChapters())
+					{
+						temp = bookArr[j];
+						bookArr[j] = bookArr[j+1];
+						bookArr[j+1] = temp;						
+					}
+                 }
+				}
+
+                return bookArr;
+			}
+    }
